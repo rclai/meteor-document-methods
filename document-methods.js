910307dd506347f68e44798d4a02493f8799851b
Richard Lai <it.rclai@gmail.com>
2015-02-02 00:54:20 -0500

    Properly flattened the arguments

--- document-methods.js
+++ document-methods.js
@@ -10,7 +10,7 @@ Meteor.startup(function () {
         delete doc._id;
         // I would _.clone or _.omit, but for some reason 
         // freaking underscore.js copies the prototypes
-        return inst.update.apply(inst, _.flatten([this._id, { $set: doc }], _.toArray(arguments)));
+        return inst.update.apply(inst, _.flatten([this._id, { $set: doc }, _.toArray(arguments)]));
       },
       $duplicate: function () {
         var doc = {};
@@ -18,7 +18,7 @@ Meteor.startup(function () {
           doc[key] = value;
         });
         delete doc._id;
-        return inst.insert.apply(inst, _.flatten([doc], _.toArray(arguments)));
+        return inst.insert.apply(inst, _.flatten([doc, _.toArray(arguments)]));
       },
       $update: function (modifier) {
         return inst.update.apply(inst, _.flatten([this._id, modifier, _.rest(_.toArray(arguments))]));
@@ -27,25 +27,25 @@ Meteor.startup(function () {
         return inst.remove.apply(inst, _.flatten([this._id, _.toArray(arguments)]));
       },
       $set: function (toSet) {
-        return inst.update.apply(inst, _.flatten([this._id, { $set: toSet }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $set: toSet }, _.rest(_.toArray(arguments))]));
       },
       $push: function (toPush) {
-        return inst.update.apply(inst, _.flatten([this._id, { $push: toPush }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $push: toPush }, _.rest(_.toArray(arguments))]));
       },
       $pushAll: function (toPush) {
-        return inst.update.apply(inst, _.flatten([this._id, { $pushAll: toPush }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $pushAll: toPush }, _.rest(_.toArray(arguments))]));
       },
       $pull: function (toPull) {
-        return inst.update.apply(inst, _.flatten([this._id, { $pull: toPull }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $pull: toPull }, _.rest(_.toArray(arguments))]));
       },
       $pullAll: function (toPull) {
-        return inst.update.apply(inst, _.flatten([this._id, { $pullAll: toPull }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $pullAll: toPull }, _.rest(_.toArray(arguments))]));
       },
       $pop: function (toPop) {
-        return inst.update.apply(inst, _.flatten([this._id, { $pop: toPop }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $pop: toPop }, _.rest(_.toArray(arguments))]));
       },
       $addToSet: function (toAdd) {
-        return inst.update.apply(inst, _.flatten([this._id, { $addToSet: toAdd }], _.rest(_.toArray(arguments))));
+        return inst.update.apply(inst, _.flatten([this._id, { $addToSet: toAdd }, _.rest(_.toArray(arguments))]));
       }      
     });
   });

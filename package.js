50c2fd038c4a69f5624cef4a6b772340365e0860
rclai <it.rclai@gmail.com>
2015-02-02 00:02:31 -0500

    Added Git URL

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Extend your documents with helpful methods.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/rclai/meteor-document-methods.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'

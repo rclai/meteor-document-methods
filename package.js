ce10ac5b021524c118652b8ae08a220fb7d60df0
Richard Lai <it.rclai@gmail.com>
2015-04-07 00:16:27 -0400

    Bumped to 0.1.13

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lai:document-methods',
-  version: '0.1.8',
+  version: '0.1.13',
   // Brief, one-line summary of the package.
   summary: 'Extend your documents with helpful methods.',
   // URL to the Git repository containing the source code for this package.
